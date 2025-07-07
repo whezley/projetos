7ad8a1fa24fff5496cc7506917d14678578ac431
whezley <francisco.w.m.q@gmail.com>
2025-07-07 19:00:39 +0000

    Declarando as variaveis

--- Inventario/inventario.c
+++ Inventario/inventario.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 
 int main() {
+//Declarando as variaveis
+    char produtoA[30] = "ProdutoA";
+    char produtoB[30] = "ProdutoB";
 
+    unsigned int estoqueA = 1000;
+    unsigned int estoqueB = 2000;
 
+    float valorA = 10.50;
+    float valorB = 20.50;
 
+    unsigned int estoqueMinimoA = 500;
+    unsigned int estoqueMinimoB = 2500;
 
-
-
-
+    double valorTotalA;
+    double valorTotalB;
+    
     
 }
